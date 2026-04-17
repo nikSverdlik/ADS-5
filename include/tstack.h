b222230b75889c8f77805bc47529b42a0fd54ab6
nikSverdlik <niksverdlik@gmail.com>
2026-04-17 23:25:43 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,48 +2,50 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
+#include <string>
+
 template<typename T, int size>
 class TStack {
-  private:
-    T arr[size];
-    int topIndex;
+ private:
+  T arr[size] = {};
+  int topIndex;
 
-  public:
-    TStack() {
-      topIndex = -1;
-    }
+ public:
+  TStack() {
+    topIndex = -1;
+  }
 
-    bool isEmpty() {
-      return topIndex == -1;
-    }
+  bool isEmpty() {
+    return topIndex == -1;
+  }
 
-    bool isFull() {
-      return topIndex == size - 1;
-    }
+  bool isFull() {
+    return topIndex == size - 1;
+  }
 
-    void push(T value) {
-      if (isFull()) {
-        throw std::string("Stack is full!");
-      }
-      topIndex = topIndex + 1;
-      arr[topIndex] = value;
+  void push(T value) {
+    if (isFull()) {
+      throw std::string("Stack is full!");
     }
+    topIndex = topIndex + 1;
+    arr[topIndex] = value;
+  }
 
-    T pop() {
-      if (isEmpty()) {
-        throw std::string("Stack is empty!");
-      }
-      T value = arr[topIndex];
-      topIndex = topIndex - 1;
-      return value;
+  T pop() {
+    if (isEmpty()) {
+      throw std::string("Stack is empty!");
     }
+    T value = arr[topIndex];
+    topIndex = topIndex - 1;
+    return value;
+  }
 
-    T top() {
-      if (isEmpty()) {
-        throw std::string("Stack is empty!");
-      }
-      return arr[topIndex];
+  T top() {
+    if (isEmpty()) {
+      throw std::string("Stack is empty!");
     }
+    return arr[topIndex];
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
